45567adcd9ee99eddb16ddfbd11c4fc2df296b23
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-05-16 12:28:32 -0500

    La calculadora funciona correctamente

--- src/App.js
+++ src/App.js
@@ -4,14 +4,48 @@ import Buttons from './Buttons';
 import './App.css'
 import { useState } from "react";
 function App() {
-  const[value, setValue] = useState('')
+  const[value, setValue] = useState('');
+
+  const ButtonClick = (buttonValue) => {
+    setValue(value + buttonValue);
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(value);
+      if (isNaN(result) || !isFinite(result)) {
+        setValue('ERROR');
+      } else {
+        setValue(result);
+      }
+    } catch (error) {
+      setValue('ERROR');
+    }
+  };
+
   return (
     <>
     <div className="App">
       <header className="App-header">
+        <h1>Calculadora básica</h1>
+      <input className='display' type='text' value={value}/>
         <div className='grid'>
-        
-        <Buttons></Buttons>
+            <Buttons name="1" onClick={ButtonClick}/>
+            <Buttons name="2" onClick={ButtonClick}/>
+            <Buttons name="3" onClick={ButtonClick}/>
+            <input type='button' className='button' value="+" onClick={() => setValue(value + '+')}/>
+            <Buttons name="4" onClick={ButtonClick}/>
+            <Buttons name="5" onClick={ButtonClick}/>
+            <Buttons name="6" onClick={ButtonClick}/>
+            <input type='button' className='button' value="-" onClick={() => setValue(value + '-')}/>
+            <Buttons name="7" onClick={ButtonClick}/>
+            <Buttons name="8" onClick={ButtonClick}/>
+            <Buttons name="9" onClick={ButtonClick}/>
+            <input type='button' className='button' value="*" onClick={() => setValue(value + '*')}/>
+            <Buttons name="0" onClick={ButtonClick}/>
+            <Buttons name="." onClick={ButtonClick}/>
+            <Buttons name="C" onClick={e => setValue('')}/>
+            <input type='button' className='button' value="=" onClick={handleCalculate}/>
         </div>
       </header>
     </div>
--- src/Buttons.js
+++ src/Buttons.js
@@ -5,11 +5,7 @@ function Buttons(props){
     const[value, setValue] = useState('')
     return(
         <>
-        <input type='text' value={value}/>
-        <input type='button' className='button' value="1" onClick={e => setValue(value + e.target.value)}/>
-        <input type='button' className='button' value="2" onClick={e => setValue(value + e.target.value)}/>
-        <input type='button' className='button' value="+" onClick={e => setValue(value + e.target.value)}/>
-        <input type='button' value="=" onClick={e => setValue(eval(value))}/>
+        <input type='button' className='button' value={props.name} onClick={() => props.onClick(props.name)}/>
         </>
 
     );
