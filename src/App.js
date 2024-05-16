e69ab2ea1dad464285c37fc2e7777fc523285311
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-05-16 11:52:52 -0500

    Ahí funciona

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './Calculator.css';
 import Buttons from './Buttons';
 import './App.css'
-
+import { useState } from "react";
 function App() {
+  const[value, setValue] = useState('')
   return (
     <>
     <div className="App">
       <header className="App-header">
         <div className='grid'>
-        <Buttons name="1"></Buttons>
+        
+        <Buttons></Buttons>
         </div>
       </header>
     </div>
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,10 +1,15 @@
-
+import { useState } from "react";
 import './Calculator.css';
 
 function Buttons(props){
+    const[value, setValue] = useState('')
     return(
         <>
-        <input type='button' className='button' value={props.name} />
+        <input type='text' value={value}/>
+        <input type='button' className='button' value="1" onClick={e => setValue(value + e.target.value)}/>
+        <input type='button' className='button' value="2" onClick={e => setValue(value + e.target.value)}/>
+        <input type='button' className='button' value="+" onClick={e => setValue(value + e.target.value)}/>
+        <input type='button' value="=" onClick={e => setValue(eval(value))}/>
         </>
 
     );
